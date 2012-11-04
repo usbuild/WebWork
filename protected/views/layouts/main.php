aaa6976707b381cf8e4dbf263351da9f644b0f2b
Qichao Zhang <njuzhangqichao@gmail.com>
2012-11-05 00:30:04 +0800

    add header css

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,6 +1,6 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
@@ -23,12 +23,21 @@
         <div id="logo_holder">
             <h1 id="logo"><a href="<?=Yii::app()->homeUrl?>">轻博客</a></h1>
         </div>
-        <div class="home-holder"><a href="<?=Yii::app()->homeUrl?>">主页</a></div>
-        <?php foreach (Common::getBlogs() as $blog): ?>
-        <div class="blog-holder"><?= CHtml::link($blog->name, array('blog/view/' . $blog->id)) ?></div>
-        <?php endforeach;?>
         <div class="opt-holder">
-            各种操作
+            <ul>
+                <li id="logout"><a href="#"></a></li>
+                <li id="setting"><a href="#"></a></li>
+                <li id="discover"><a href="#"></a></li>
+            </ul>
+        </div>
+        <div class="link-holder">
+            <ul>
+                <li><a href="<?=Yii::app()->homeUrl?>" class="active">主页</a></li>
+                <?php foreach (Common::getBlogs() as $blog): ?>
+                <li><?= CHtml::link($blog->name, array('blog/view/' . $blog->id)) ?></li>
+                <?php endforeach;?>
+                <li><a href="<?=Yii::app()->homeUrl?>" id="add_blog"></a></li>
+            </ul>
         </div>
     </div>
 </div>
