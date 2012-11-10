19934fc769aa3e4f6633cc31eeb4c97077736254
Usbuild <njuzhangqichao@gmail.com>
2012-11-10 20:17:43 +0800

    finish repost system and hot count

---
+++ protected/controllers/TestController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: usbuild
+ * Date: 12-11-10
+ * Time: 下午7:40
+ * To change this template use File | Settings | File Templates.
+ */
+class TestController
+{
+
+}
