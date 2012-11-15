59c7f62d5058fc0124f706b6ac513d834760b0f4
Qichao Zhang <njuzhangqichao@gmail.com>
2012-11-15 09:27:56 +0800

    add post image

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,10 +1,26 @@
-<ul class="m-nav2">
-    <li class="user"><a href="#"></a></li>
-    <li><a class="text" href="<?=$this->createUrl('post/text');?>" title="文字"></a></li>
-    <li><a class="photo" href="<?=$this->createUrl('post/photo');?>" title="照片"></a></li>
-    <li><a class="music" href="<?=$this->createUrl('post/music');?>" title="音乐"></a></li>
-    <li><a class="video" href="<?=$this->createUrl('post/video');?>" title="视频"></a></li>
-</ul>
+<div id="publisher" class="publisher clearfix">
+    <div class="pb-avatar">
+        <a class="blog-avatar" href="#"
+           style="background-image:url(<?=Yii::app()->baseurl . Yii::app()->user->model->myblog->avatar?>)">师乐园</a>
+        <span id="upload-avatar-when-no-avatar" uploadid="4" class="frameupload-hook"
+              style="cursor: default;">+博客头像</span>
+    </div>
+    <div class="pb-action-holder">
+        <div class="pb-triangle"></div>
+        <div class="pb-action-shadow-top"></div>
+        <div class="pb-action" id="pb-action">
+            <ul class="clearfix">
+                <li><a class="text" href="<?=$this->createUrl('post/text')?>">文字</a></li>
+                <li><a class="photo" href="<?=$this->createUrl('post/photo')?>">图片</a></li>
+                <li><a class="music" href="<?=$this->createUrl('post/music')?>">声音</a></li>
+                <li><a class="video" href="<?=$this->createUrl('post/video')?>">影像</a></li>
+                <li><a class="link" href="<?=$this->createUrl('post/link')?>">链接</a></li>
+            </ul>
+        </div>
+        <div class="pb-action-shadow-bottom"></div>
+    </div>
+</div>
+
 <input type="hidden" id="fetch_url" value="post/getposts">
 
 <div class="feed-zone" id="feed_zone">
@@ -12,4 +28,5 @@
 </div>
 
 <img id="loading_more" src="<?=Yii::app()->baseUrl?>/images/loading-more.gif"/>
+
 <div id="back_to_top" class="scroll-to-top"></div>
