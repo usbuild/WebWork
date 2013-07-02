118ce4c72db80cdccaaba2f4d3a1e6a51a38609c
Qichao Zhang <njuzhangqichao@gmail.com>
2013-07-02 10:56:39 +0800

    add error page

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,13 +1,56 @@
 <html>
 <head>
-    <title><?php $this->pageTitle = Yii::app()->name . ' - Error'; ?></title>
+    <title>出错啦!</title>
+    <style type="text/css">
+        .error-title {
+            margin-bottom: 20px;
+            font-weight: bold;
+            font-size: 20px;
+            margin-left: 20px;
+        }
+
+        .error-detail {
+
+            border: solid lightgray 1px;
+            border-radius: 5px;
+            text-align: center;
+
+            padding: 40px;
+        }
+
+        .container {
+            width: 600px;
+            margin: 100px auto;
+        }
+
+        .error-num {
+            color: red;
+            font-size: 25px;
+            font-weight: bold;
+        }
+
+        .operation {
+            margin-top: 20px;
+            font-size: 13px;
+        }
+
+        .operation a {
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
+<div class="container">
+    <div class="error-title">访问出错啦</div>
 
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-    <?php echo CHtml::encode($message); ?>
+    <div class="error-detail">
+        <span class="error-num"><?php echo $code; ?></span>
+        <hr>
+        <?php echo CHtml::encode($message); ?>
+        <div class="operation">
+            <a href="<?=Yii::app()->createUrl('/')?>">回到首页</a> 或 <a href="javascript:history.go(-1)">返回上页</a>
+        </div>
+    </div>
 </div>
 </body>
 </html>
